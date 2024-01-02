2860e9b1b9cf77a1e030e9e8482cbe2c8feaf944
casarin <nsgbanks@ciencias.unam.mx>
2024-01-01 21:51:16 -0600

    Calculator updated to start comparison between strings

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.text.Collator;
 import com.fasterxml.jackson.databind.JsonNode;
-
 
 public class Calculator {
     
@@ -9,6 +10,22 @@ public class Calculator {
 
     public Calculator(JsonNode input) {
 	this.name_list = new ArrayList<String>();
+
+	// It is necessary to traverse twice because input is a list of jsons.
+	for (JsonNode names: input) {
+	    for (JsonNode name : names) {
+		name_list.add(name.toString());
+	    }
+	}
+
+	name_list.sort(new Comparator<String>() {
+		@Override public int compare(String s1, String s2) {
+		    Collator collator = Collator.getInstance();
+		    return 1;
+		}
+	    });
+
+	
     }
 
     public int getTotalScore() {
